75d7a91a5ac21bc0fdceaba0e3ffd53613be5541
peperoschach <luiz.felipe_23@yahoo.com.br>
2017-11-15 00:38:19 -0200

    Creating Component Rating

--- out-tsc/app/app/shared/rating/rating.component.js
+++ out-tsc/app/app/shared/rating/rating.component.js
@@ -7,9 +7,10 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 var RatingComponent = (function () {
     function RatingComponent() {
+        this.rated = new EventEmitter();
         this.rates = [1, 2, 3, 4, 5];
         this.rate = 0;
     }
@@ -17,9 +18,27 @@ var RatingComponent = (function () {
     };
     RatingComponent.prototype.setRate = function (r) {
         this.rate = r;
+        this.previousRate = undefined;
+        this.rated.emit(this.rate);
+    };
+    RatingComponent.prototype.setTemporaryRate = function (r) {
+        if (this.previousRate === undefined) {
+            this.previousRate = this.rate;
+        }
+        this.rate = r;
+    };
+    RatingComponent.prototype.clearTemporaryRate = function () {
+        if (this.previousRate !== undefined) {
+            this.rate = this.previousRate;
+            this.previousRate = undefined;
+        }
     };
     return RatingComponent;
 }());
+__decorate([
+    Output(),
+    __metadata("design:type", Object)
+], RatingComponent.prototype, "rated", void 0);
 RatingComponent = __decorate([
     Component({
         selector: 'mt-rating',
